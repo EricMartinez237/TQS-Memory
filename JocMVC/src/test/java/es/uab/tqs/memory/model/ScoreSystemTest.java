0efa0acc5dd388b1c0ee5d217e2a11679ac52f37
EricMartinez237 <1636451@uab.cat>
2025-11-16 00:09:36 +0100

    tests a funció addPoints amb valors limit/frontera

--- JocMVC/src/test/java/es/uab/tqs/memory/model/ScoreSystemTest.java
+++ JocMVC/src/test/java/es/uab/tqs/memory/model/ScoreSystemTest.java
@@ -29,10 +29,60 @@ public class ScoreSystemTest extends TestCase {
     //Valor negatiu
     public void testAfegirPuntsNegatius() {
         ScoreSystem sistema = new ScoreSystem();
-        sistema.addPoints(-5);
+        sistema.addPoints(5);
+        sistema.addPoints(-2);
+        assertEquals(3, sistema.getPoints());
+    }
+
+    // Tests de valors límit
+
+    // Valor límit: 1
+    public void testPuntMinimPositiu() {
+        ScoreSystem sistema = new ScoreSystem();
+        sistema.addPoints(1);
+        assertEquals(1, sistema.getPoints());
+    }
+
+    // Valor límit: -1 (mínim negatiu)
+    public void testPuntMinimNegatiu() {
+        ScoreSystem sistema = new ScoreSystem();
+        sistema.addPoints(5);
+        sistema.addPoints(-1);
+        assertEquals(4, sistema.getPoints());
+    }
+
+    // Valor límit: zero després de tenir punts
+    public void testAfegirZeroAmbPuntsExistents() {
+        ScoreSystem sistema = new ScoreSystem();
+        sistema.addPoints(10);
+        sistema.addPoints(0);
+        assertEquals(10, sistema.getPoints());
+    }
+
+    // Cas frontera: restar exactament els punts que es tenen
+    public void testRestarExactamentElsPuntsActuals() {
+        ScoreSystem sistema = new ScoreSystem();
+        sistema.addPoints(7);
+        sistema.addPoints(-7);
         assertEquals(0, sistema.getPoints());
     }
 
+    //Valor negatiu desde l'inici
+    public void testPuntsNegatiusDesdeZero() {
+        ScoreSystem sistema = new ScoreSystem();
+        sistema.addPoints(-10);
+        assertEquals(0, sistema.getPoints());
+    }
+
+    //Valor negatiu després de restar
+    public void testPuntsNoSonNegatius() {
+        ScoreSystem sistema = new ScoreSystem();
+        sistema.addPoints(3);
+        sistema.addPoints(-5);
+        assertEquals(0, sistema.getPoints());
+    }
+    
+    
     public void testIncrementAttempts() {
         ScoreSystem scoreSystem = new ScoreSystem();
         
