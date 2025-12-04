83e428bceb40d1e26a17935f8d72d34ac601ee75
EricMartinez237 <1636451@uab.cat>
2025-12-04 17:58:42 +0100

    AppTest creat - primer commit app

---
+++ src/test/java/es/uab/tqs/memory/AppTest.java
@@ -0,0 +1,13 @@
+package test.java.es.uab.tqs.memory;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testAppinicialitzation(){
+        assertTrue(true, "La aplicació s'inicialitza sense errors");
+    }
+} 
+
